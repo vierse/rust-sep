caf37494a9b32d89a6d3b819ce7544ce68308e6e
Efe Kaan Salıcı <efekaansalici@Efe-MacBook-Pro.local>
2025-12-17 23:38:13 +0300

    implement the exists alias function db

--- src/db.rs
+++ src/db.rs
@@ -1,22 +1,79 @@
 use anyhow::Result;
 use async_trait::async_trait;
+use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
+
+const INIT_QUERY: &str = r#"
+CREATE TABLE IF NOT EXISTS aliases (
+    alias TEXT PRIMARY KEY,
+    url   TEXT NOT NULL
+);
+"#;
 
 #[async_trait]
 pub trait Database {
     async fn insert(&self, alias: &str, url: &str) -> Result<()>;
 
     async fn get(&self, alias: &str) -> Result<String>;
+
+    async fn exists(&self, alias: &str) -> Result<bool>;
 }
 
-pub struct SqliteDB {}
+#[derive(Clone)]
+pub struct SqliteDB {
+      pub pool: SqlitePool,
+}
+
+impl SqliteDB {
+    pub async fn new(database_url: &str) -> Result<Self> {
+        let pool = SqlitePoolOptions::new()
+            .max_connections(2)
+            .connect(database_url)
+            .await?;
+
+        sqlx::query(INIT_QUERY).execute(&pool).await?;
+
+        Ok(Self { pool })
+    }
+}
 
 #[async_trait]
 impl Database for SqliteDB {
-    async fn insert(&self, _alias: &str, _url: &str) -> Result<()> {
-        unimplemented!()
+    async fn insert(&self, alias: &str, url: &str) -> Result<()> {
+        sqlx::query(
+            "INSERT INTO aliases (alias, url) VALUES (?, ?)"
+        )
+        .bind(alias)
+        .bind(url)
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
     }
 
-    async fn get(&self, _alias: &str) -> Result<String> {
-        unimplemented!()
+async fn get(&self, alias: &str) -> Result<String> {
+        let url = sqlx::query_scalar::<_, String>("SELECT url FROM aliases WHERE alias = ?")
+            .bind(alias)
+            .fetch_one(&self.pool)
+            .await?;
+
+        Ok(url)
     }
-}
+
+async fn exists(&self, alias: &str) -> Result<bool> 
+    {
+      let exist =sqlx::query_scalar::<_ , i32>(
+            r#"
+            SELECT 1
+            FROM aliases
+            WHERE alias = ? ;
+           "#
+          )
+    .bind(alias)
+    .fetch_optional(&self.pool)
+    .await?;
+        
+    Ok(exist.is_some())
+    
+
+
+}  } 
