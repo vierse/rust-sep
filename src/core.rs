7b1aa6a785625a598312ed6226630d8259dccd35
Efe Kaan Salıcı <efekaansalici@Efe-MacBook-Pro.local>
2025-12-18 15:14:28 +0300

    adjustment to the core.rs

--- src/core.rs
+++ src/core.rs
@@ -1,49 +1,49 @@
-use std::sync::Arc;
+    use std::sync::Arc;
 
-use anyhow::Result;
-use async_trait::async_trait;
-use tokio::net::TcpListener;
+    use anyhow::Result;
+    use async_trait::async_trait;
+    use tokio::net::TcpListener;
 
-use crate::{
-    api::build_router,
-    db::{Database, SqliteDB},
-};
+    use crate::{
+        api::build_router,
+        db::{Database, SqliteDB},
+    };
 
-#[async_trait]
-pub trait BaseApp {
-    async fn create_alias(&self, url: &str) -> Result<String>;
+    #[async_trait]
+    pub trait BaseApp {
+        async fn create_alias(&self, url: &str) -> Result<String>;
 
-    async fn get_url(&self, alias: &str) -> Result<String>;
-}
-
-#[derive(Clone)]
-pub struct AppState {
-    pub app: Arc<dyn BaseApp + Send + Sync>,
-}
-
-pub struct App {
-    _db: Arc<dyn Database + Send + Sync>,
-}
-
-#[async_trait]
-impl BaseApp for App {
-    async fn create_alias(&self, _url: &str) -> Result<String> {
-        unimplemented!()
+        async fn get_url(&self, alias: &str) -> Result<String>;
     }
 
-    async fn get_url(&self, _alias: &str) -> Result<String> {
-        unimplemented!()
+    #[derive(Clone)]
+    pub struct AppState {
+        pub app: Arc<dyn BaseApp + Send + Sync>,
     }
-}
 
-pub async fn run() -> Result<()> {
-    let db = Arc::new(SqliteDB {});
-    let app = Arc::new(App { _db: db });
-    let router = build_router(AppState { app });
+    pub struct App {
+        _db: Arc<dyn Database + Send + Sync>,
+    }
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    #[async_trait]
+    impl BaseApp for App {
+        async fn create_alias(&self, _url: &str) -> Result<String> {
+            unimplemented!()
+        }
 
-    axum::serve(listener, router).await.unwrap();
+        async fn get_url(&self, _alias: &str) -> Result<String> {
+            unimplemented!()
+        }
+    }
 
-    Ok(())
-}
+    pub async fn run() -> Result<()> {
+        let db = Arc::new(SqliteDB::new("sqlite:dbfile.db?mode=rwc").await?);
+        let app = Arc::new(App { _db: db });
+        let router = build_router(AppState { app });
+
+        let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+        axum::serve(listener, router).await.unwrap();
+
+        Ok(())
+    }
