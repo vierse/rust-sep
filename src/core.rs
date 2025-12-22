076edd87ee3a1b0bbdca0e34697dfa8ac7d432be
NotBot-Computer <berkant.k2005@gmail.com>
2025-12-22 17:46:30 +0100

    rename db

--- src/core.rs
+++ src/core.rs
@@ -23,7 +23,7 @@ pub struct AppState {
 }
 
 pub struct App {
-    _db: Arc<dyn Database + Send + Sync>,
+    db: Arc<dyn Database + Send + Sync>,
 }
 
 fn generate_alias() -> String {
@@ -42,14 +42,14 @@ impl BaseApp for App {
         for _ in 0..MAX_RETRIES {
             let alias = generate_alias();
 
-            if self._db.get(&alias).await.is_ok() {
+            if self.db.get(&alias).await.is_ok() {
                 continue;
             }
 
-            match self._db.insert(&alias, url).await {
+            match self.db.insert(&alias, url).await {
                 Ok(()) => return Ok(alias),
                 Err(e) => {
-                    if self._db.get(&alias).await.is_ok() {
+                    if self.db.get(&alias).await.is_ok() {
                         continue;
                     }
                     return Err(e);
@@ -64,14 +64,14 @@ impl BaseApp for App {
     }
 
     async fn get_url(&self, alias: &str) -> Result<String> {
-        let url = self._db.get(alias).await?;
+        let url = self.db.get(alias).await?;
         Ok(url)
     }
 }
 
 pub async fn run() -> Result<()> {
     let db = Arc::new(SqliteDB {});
-    let app = Arc::new(App { _db: db });
+    let app = Arc::new(App { db: db });
     let router = build_router(AppState { app });
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
