d84cffb12c69047162e02ec3153014f17fd20f3e
vierse <83981418+vierse@users.noreply.github.com>
2025-12-29 18:41:18 +0100

    build a docker image #12

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // route to axum (dev only)
+        target: 'http://api:3000', // route to axum (dev only)
         changeOrigin: true,
       },
     },
