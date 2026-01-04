e85fe9a065146295bda0b7ad8d58dd3093324ed2
lenawanel <lenawanel@protonmail.com>
2026-01-04 18:52:56 +0100

    add basic hitcount tracking

--- migrations/20251230102523_create_links_table.sql
+++ migrations/20251230102523_create_links_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE links (
     id BIGSERIAL PRIMARY KEY,
     alias TEXT UNIQUE,
     url TEXT NOT NULL,
+    -- TODO: look into ON, and other datatypes
+    hitcount BIGINT DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
