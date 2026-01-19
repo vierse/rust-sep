66b2cdef19f49e8e0e12f264e0f0dc19e7568d64
vierse <83981418+vierse@users.noreply.github.com>
2026-01-19 13:28:55 +0100

    added last access timestamp to metrics

--- migrations/20260118214749_create_daily_hits_table.sql
+++ migrations/20260118214749_create_daily_hits_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE daily_hits (
     day DATE NOT NULL,
     link_id BIGINT NOT NULL REFERENCES links(id) ON DELETE CASCADE,
     hits BIGINT NOT NULL,
+    last_access TIMESTAMPTZ NOT NULL,
     PRIMARY KEY (day, link_id)
 ) PARTITION BY RANGE (day);
--- src/metrics.rs
+++ src/metrics.rs
@@ -5,11 +5,17 @@ use std::sync::{
 
 use arc_swap::ArcSwap;
 use dashmap::DashMap;
+use time::OffsetDateTime;
 
-pub type MetricsMap = DashMap<i64, AtomicI64>;
+pub type MetricsMap = DashMap<i64, MetricsValue>;
 
 pub struct Metrics {
     current: ArcSwap<MetricsMap>,
+}
+
+pub struct MetricsValue {
+    hits: AtomicI64,
+    last_access_s: AtomicI64,
 }
 
 impl Metrics {
@@ -20,14 +26,47 @@ impl Metrics {
     }
 
     pub fn record_hit(&self, link_id: i64) {
+        let now_s = OffsetDateTime::now_utc().unix_timestamp();
+
         let map = self.current.load();
+        let val = map.entry(link_id).or_insert(MetricsValue::new(now_s));
 
-        let val = map.entry(link_id).or_insert(AtomicI64::new(1));
+        // increment hitcount
+        val.hits.fetch_add(1, Ordering::Relaxed);
 
-        val.fetch_add(1, Ordering::Relaxed);
+        // update last access timestamp
+        let mut last_access_s = val.last_access_s.load(Ordering::Relaxed);
+        while now_s > last_access_s {
+            match val.last_access_s.compare_exchange_weak(
+                last_access_s,
+                now_s,
+                Ordering::Relaxed,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => break,
+                Err(next) => last_access_s = next,
+            }
+        }
     }
 
     pub fn swap_map(&self) -> Arc<MetricsMap> {
         self.current.swap(Arc::new(DashMap::new()))
+    }
+}
+
+impl MetricsValue {
+    pub fn new(last_access_s: i64) -> Self {
+        Self {
+            hits: AtomicI64::new(1),
+            last_access_s: AtomicI64::new(last_access_s),
+        }
+    }
+
+    pub fn hits(&self) -> i64 {
+        self.hits.load(Ordering::Relaxed)
+    }
+
+    pub fn last_access_s(&self) -> i64 {
+        self.last_access_s.load(Ordering::Relaxed)
     }
 }
