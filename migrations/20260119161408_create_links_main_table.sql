d40d9098f2e344d6df7366d0cfa6810c9880035a
Efe Kaan Salıcı <efekaansalici07@gmail.com>
2026-02-13 19:07:42 +0100

    move password column into a new migration and fix for the unneccesary creation of hasher

--- migrations/20260119161408_create_links_main_table.sql
+++ migrations/20260119161408_create_links_main_table.sql
@@ -3,6 +3,5 @@ CREATE TABLE links_main (
     id BIGSERIAL PRIMARY KEY,
     alias TEXT UNIQUE,
     url TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    password_hash TEXT
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
