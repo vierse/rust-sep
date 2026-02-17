36558ee1bb1980c61658ada1057d1f508f7c3856
lenawanel <lenawanel@protonmail.com>
2026-02-17 18:34:01 +0100

    apply suggestions

--- src/app/usage_metrics.rs
+++ src/app/usage_metrics.rs
@@ -1,137 +1,127 @@
-use std::sync::atomic::{AtomicUsize, Ordering::Acquire};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use time::OffsetDateTime;
 
 #[derive(Default)]
 pub struct MetricsDay {
-    hours: [Hour; 24],
+    pub hours: [Hour; 24],
 }
 
 #[derive(Default)]
 pub struct Metrics {
-    week_days: [MetricsDay; 7],
-    // woul be 31 * 12 * 24 * 16 bytes
-    // months: Box<[[Day; 31]; 12]>,
+    pub week_days: [MetricsDay; 7],
 }
 
-macro_rules! tracked_categories {
-    ($($cat:ident, $cat_camel:ident);*) => {
-        #[derive(Default)]
-        pub struct Hour {
-            $($cat: AtomicUsize,)*
-        }
-
-        #[derive(Clone, Copy)]
-        pub enum Category {
-            $($cat_camel,)*
-        }
-
-        impl Metrics {
-            pub async fn log(&self, cat: Category) {
-                let date_time = OffsetDateTime::now_utc();
-                let date = date_time.date();
-                let time = date_time.time();
-
-                let week_day = date.weekday().number_from_monday() as usize;
-                // let month_day = date.day() as usize;
-                // let month = date.month() as usize;
-
-                let hour = time.hour() as usize;
-
-                match cat {
-                    $(
-                        Category::$cat_camel =>
-                            self.week_days[week_day].hours[hour]
-                                .$cat
-                                .fetch_add(1, std::sync::atomic::Ordering::AcqRel),
-                    )*
-                };
-            }
-
-            pub fn most_frequented_day_cat(&self, cat: Category) -> usize {
-                match cat {
-                    $(
-                        Category::$cat_camel =>
-                            {
-                                let (idx, _reds) =
-                                                self
-                                                .week_days
-                                                .iter()
-                                                .map(|day| day.total_usage_in(cat))
-                                                .enumerate()
-                                                .max_by_key(|(_, reds)| *reds)
-                                                // this should never panick as self.week_days is a fixed size array
-                                                .unwrap();
-                            idx
-                        },
-                    )*
-                }
-            }
-
-            pub fn total_usage_in(&self, cat: Category) -> usize {
-                use Category::*;
-                match cat {
-                    $(
-                        $cat_camel => self.week_days.iter().map(|day| day.total_usage_in(cat)).sum(),
-                    )*
-                }
-            }
-        }
-
-        impl MetricsDay {
-            pub fn avg_hourly_redirects(&self, cat: Category) -> f64 {
-                self.total_usage_in(cat) as f64 / self.hours.len() as f64
-            }
-
-            pub fn total_usage_in(&self, cat: Category) -> usize {
-                match cat {
-                    $(
-                        Category::$cat_camel => {
-                            self.hours.iter().map(|h| h.$cat.load(Acquire)).sum()
-                        }
-                    )*
-                }
-            }
-
-            /// returns the hour that has seen the most redirects
-            pub fn most_usage(&self, cat: Category) -> usize {
-                let (idx, _reds) = match cat {
-                    $(
-                        Category::$cat_camel =>
-                             self
-                                .hours
-                                .iter()
-                                .map(|h| h.redirect.load(Acquire))
-                                .into_iter()
-                                .enumerate()
-                                .max_by_key(|(_, reds)| *reds)
-                                // this should never panick as self.hours is a fixed size array
-                                .unwrap(),
-                    )*
-                };
-                idx
-            }
-
-            /// computes the fraction the given hour has seen of all redirects
-            pub fn usage_fraction(&self, hour: usize, cat: Category) -> anyhow::Result<f64> {
-                anyhow::ensure!(hour < self.hours.len(), "given hour doesn't fit in a day");
-
-                Ok(self.hours[hour].redirect.load(Acquire) as f64 / self.total_usage_in(cat) as f64)
-            }
-
-            pub fn usage(&self, hour: usize, cat: Category) -> anyhow::Result<usize> {
-                anyhow::ensure!(hour < self.hours.len(), "given hour doesn't fit in a day");
-
-                match cat {
-                    $(
-                        Category::$cat_camel =>
-                            Ok(self.hours[hour].$cat.load(Acquire)),
-                    )*
-                }
-            }
-        }
-    };
-
+#[derive(Default)]
+pub struct Hour {
+    pub categories: [AtomicUsize; 6],
 }
 
-tracked_categories!(redirect, Redirect; recent, Recent; shorten, Shorten; recently_added, RecentlyAdded;
-                    authenticate_session, AuthenticateSession; authenticate_user, AuthenticateUser);
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum Category {
+    Redirect,
+    Recent,
+    Shorten,
+    RecentlyAdded,
+    AuthenticateSession,
+    AuthenticateUser,
+}
+
+impl Metrics {
+    pub fn log(&self, cat: Category) {
+        let date_time = OffsetDateTime::now_utc();
+        let date = date_time.date();
+        let time = date_time.time();
+        let week_day = date.weekday().number_from_monday() as usize;
+        let hour = time.hour() as usize;
+
+        self.week_days[week_day].hours[hour].categories[cat as usize]
+            .fetch_add(1, Ordering::Relaxed);
+    }
+
+    /// computes the day which saw the most hits in a given category
+    pub fn most_frequented_weekday_in(&self, cat: Category) -> usize {
+        let (idx, _) = self
+            .week_days
+            .iter()
+            .map(|day| day.total_usage_in(cat))
+            .enumerate()
+            .max_by_key(|(_, reds)| *reds)
+            .unwrap();
+        idx
+    }
+    pub fn total_usage_in(&self, cat: Category) -> usize {
+        self.week_days
+            .iter()
+            .map(|day| day.total_usage_in(cat))
+            .sum()
+    }
+}
+
+impl MetricsDay {
+    pub fn avg_hourly_redirects(&self, cat: Category) -> f64 {
+        self.total_usage_in(cat) as f64 / self.hours.len() as f64
+    }
+
+    pub fn total_usage_in(&self, cat: Category) -> usize {
+        self.hours
+            .iter()
+            .map(|h| h.categories[cat as usize].load(Ordering::Relaxed))
+            .sum()
+    }
+
+    pub fn total_usage(&self) -> usize {
+        self.hours.iter().map(|h| h.sum()).sum()
+    }
+
+    /// returns the hour that has seen the most hits in a category
+    pub fn most_hit_hour(&self, cat: Category) -> usize {
+        let (idx, _reds) = self
+            .hours
+            .iter()
+            .map(|h| h.categories[cat as usize].load(Ordering::Relaxed))
+            .into_iter()
+            .enumerate()
+            .max_by_key(|(_, reds)| *reds)
+            .unwrap();
+
+        idx
+    }
+
+    pub fn most_hits_total(&self) -> usize {
+        let (idx, _reds) = self
+            .hours
+            .iter()
+            .map(|h| h.sum())
+            .into_iter()
+            .enumerate()
+            .max_by_key(|(_, reds)| *reds)
+            .unwrap();
+
+        idx
+    }
+
+    /// returns the usage a category in an hour /  total_occurances
+    pub fn usage_frequency_in(&self, hour: usize, cat: Category) -> anyhow::Result<f64> {
+        anyhow::ensure!(hour < self.hours.len(), "given hour doesn't fit in a day");
+
+        Ok(
+            self.hours[hour].categories[cat as usize].load(Ordering::Relaxed) as f64
+                / self.total_usage_in(cat) as f64,
+        )
+    }
+
+    pub fn usage(&self, hour: usize, cat: Category) -> anyhow::Result<usize> {
+        anyhow::ensure!(hour < self.hours.len(), "given hour doesn't fit in a day");
+
+        Ok(self.hours[hour].categories[cat as usize].load(Ordering::Relaxed))
+    }
+}
+
+impl Hour {
+    pub fn sum(&self) -> usize {
+        self.categories
+            .iter()
+            .fold(0, |acc, e| acc + e.load(Ordering::Relaxed))
+    }
+}
