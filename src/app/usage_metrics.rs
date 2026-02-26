bcc5651f780203a096dead86cf914364b4097dd7
lenawanel <lenawanel@protonmail.com>
2026-02-26 14:51:31 +0100

    add ability to query only specific hours

--- src/app/usage_metrics.rs
+++ src/app/usage_metrics.rs
@@ -84,6 +84,36 @@ impl Default for CategorySet {
     }
 }
 
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+/// 24 hours => 24 bits
+pub struct HourSet(u32);
+
+impl HourSet {
+    pub fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn set(&mut self, hour: usize) -> Self {
+        self.0 |= 1u32.wrapping_shl(hour as u32);
+        *self
+    }
+
+    pub fn is_set(&self, hour: usize) -> bool {
+        self.0 & 1u32.wrapping_shl(hour as u32) != 0
+    }
+
+    pub fn from_raw(cats: u32) -> Self {
+        Self(cats)
+    }
+}
+
+impl Default for HourSet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Metrics {
     pub fn log(&self, cat: Category) {
         let date_time = OffsetDateTime::now_utc();
@@ -119,6 +149,12 @@ impl Metrics {
         self.week_days.iter().map(|d| d.total_usage())
     }
 
+    pub fn total_usage_daily_in_hours(&self, hours: HourSet) -> impl Iterator<Item = usize> {
+        self.week_days
+            .iter()
+            .map(move |d| d.total_usage_in_hours(hours))
+    }
+
     pub fn total_usage_by_day_in_bitset(
         &self,
         cat: CategorySet,
@@ -126,6 +162,16 @@ impl Metrics {
         self.week_days
             .iter()
             .map(move |day| day.total_usage_in_bitset(cat))
+    }
+
+    pub fn total_usage_by_day_in_hours_bitset(
+        &self,
+        cats: CategorySet,
+        hours: HourSet,
+    ) -> impl Iterator<Item = impl Iterator<Item = usize>> {
+        self.week_days
+            .iter()
+            .map(move |day| day.total_usage_in_hours_bitset(cats, hours))
     }
 }
 
@@ -153,8 +199,41 @@ impl MetricsDay {
             })
     }
 
+    pub fn total_usage_in_hours_bitset(
+        &self,
+        cats: CategorySet,
+        hours: HourSet,
+    ) -> impl Iterator<Item = usize> {
+        (0..=Category::AuthenticateUser as u32)
+            .filter(move |idx| cats.is_set_raw(*idx))
+            .map(move |idx| {
+                self.hours
+                    .iter()
+                    .enumerate()
+                    .filter_map(move |(hour_idx, cs)| {
+                        if hours.is_set(hour_idx) {
+                            Some(cs)
+                        } else {
+                            None
+                        }
+                    })
+                    // TODO: turn the access into column major
+                    .map(|h| h.categories[idx as usize].load(Ordering::Relaxed))
+                    .sum()
+            })
+    }
+
     pub fn total_usage(&self) -> usize {
         self.hours.iter().map(|h| h.sum()).sum()
+    }
+
+    pub fn total_usage_in_hours(&self, hours: HourSet) -> usize {
+        self.hours
+            .iter()
+            .enumerate()
+            .filter(|&(hour_idx, _)| hours.is_set(hour_idx))
+            .map(|(_, h)| h.sum())
+            .sum()
     }
 
     /// returns the hour that has seen the most hits in a category
